49bd04260cb99b730e5e3eb62b59f3dee6e6f2a8
Manuel Kaufmann <humitos@gmail.com>
2013-09-16 15:01:39 -0300

    improvement in replace_collection script

--- nikola/utils/replace_collections.py
+++ nikola/utils/replace_collections.py
@@ -5,13 +5,13 @@ import glob
 import shutil
 
 SEARCH_FOR = '[gallery '
-REPLACE_RE = r'\[gallery.*\]'
+REPLACE_RE = '\\[gallery.*\n?.*\\]'
 FIND_RE = r'ids="(.*)"'
 # \1 ids
 
-REPLACE_CONTENT = r'''.. slides::\n\n    %s'''
+REPLACE_CONTENT = '''.. image:: %(image)s\n   :target: %(target)s\n   :height: 135px\n'''
 OLD_FILES_PATH = '/media/humitos/Toshiba/backup-desktop/blog-wordpress-nikola/files'
-FILES_PATH = '/home/humitos/blog/files'
+FILES_PATH = '/home/humitos/blog/nikola/files'
 
 csvf = '/home/humitos/blog/nikola/utils/attachments_ids.csv'
 idsf = csv.reader(open(csvf, 'r'), delimiter=',')
@@ -31,25 +31,26 @@ for f in glob.glob('*.rst'):
         ids = re.findall(FIND_RE, content)[0].split(',')
         file_ids = [IDS[i] for i in ids]
 
+        images = []
+        for i in file_ids:
+            year, month = re.search(r'(\d{4})/(\d{2})', i).groups()
 
-        slides = '\n    '.join(file_ids)
-        new_content = re.sub(content, REPLACE_RE, REPLACE_CONTENT % slides, re.MULTILINE)
+            i = re.sub(r'(\d{4}/\d{2}/)', '', i.replace('http://humitos.files.wordpress.com/', ''))
+            filename, ext = os.path.splitext(i)
+            image = filename + '.thumbnail' + ext
+            images.append(REPLACE_CONTENT % {'image': image, 'target': i})
+            destination = os.path.join(FILES_PATH, 'posts', 'wordpress', f[:-4])
+            source = os.path.join(OLD_FILES_PATH, year, month, i)
+            if not os.path.exists(destination):
+                os.mkdir(destination)
+            shutil.copy(source, destination)
+
+        images = '\n'.join(images)
+        new_content = re.sub(REPLACE_RE, images, content)
 
         import ipdb;ipdb.set_trace()
-        # fd = open(f, 'w')
-        # fd.write(new_content)
-        # fd.close()
-
-        groups = re.search(REPLACE_RE, content).groups()
-        os.mkdir(os.path.join(FILES_PATH, f[:-4]))
-        source = os.path.join(
-            OLD_FILES_PATH, groups[0], groups[1], groups[2])
-        destination = os.path.join(FILES_PATH, f[:-4])
-
-        if '?' in source:
-            # remove: ?w=580
-            source = source[:-6]
-
-        shutil.copy(source, destination)
+        fd = open(f, 'w')
+        fd.write(new_content)
+        fd.close()
 
         break
