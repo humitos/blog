b09abc8e549a7d9d2a1fd77baca968d40ba41cf0
Manuel Kaufmann <humitos@gmail.com>
2014-04-22 12:00:55 -0300

    License changed

--- nikola/conf.py
+++ nikola/conf.py
@@ -301,16 +301,15 @@ FAVICONS = {
 # I recommend using the Creative Commons' wizard:
 # http://creativecommons.org/choose/
 LICENSE = """
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/ar/">
-<img alt="Creative Commons License BY-NC-SA"
-style="border-width:0; margin-bottom:12px;"
-src="http://i.creativecommons.org/l/by-nc-sa/2.5/ar/88x31.png"></a>"""
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+  <img alt="Licencia Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+</a>"""
 
 # A small copyright notice for the page footer (in HTML).
 # Default is ''
-CONTENT_FOOTER = '&copy; 2007-{date} <a href="mailto:{email}">{author}</a> - Powered by <a href="http://nikola.ralsina.com.ar">Nikola</a>' + \
-    '<br/>El contenido de éste blog está bajo la licencia Creative Commons,' + \
-    '<br/>la misma está disponible completa <a href="http://creativecommons.org/licenses/by-nc-sa/2.5/ar/legalcode">aquí.</a>' + \
+CONTENT_FOOTER = '&copy; 2007-{date} <a href="mailto:{email}">{author}</a> - Powered by <a href="http://getnikola.com">Nikola</a>' + \
+    '<br/>El contenido de este blog está bajo la licencia Creative Commons,' + \
+    '<br/>la misma está disponible completa <a target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">aquí.</a>' + \
     '<br/>{license}'
 CONTENT_FOOTER = CONTENT_FOOTER.format(email=BLOG_EMAIL,
                                        author=BLOG_AUTHOR,
