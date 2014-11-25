5218449715e29d1b4243b018e70b0c1995c8b1e9
Manuel Kaufmann <humitos@gmail.com>
2014-11-25 13:32:08 -0300

    Icon on map

--- nikola/themes/elblogdehumitos/assets/js/geolocation.js
+++ nikola/themes/elblogdehumitos/assets/js/geolocation.js
@@ -2,6 +2,17 @@ $(document).ready(function (){
     // check if the map id exists before executing the code
     if ($('#map').length) {
 	$.getJSON("/assets/js/point.json", function(data){
+	    var icon = L.icon({
+		iconUrl: '/assets/img/marker.png',
+		shadowUrl: '/assets/img/marker-shadow.png',
+
+		iconSize:     [64, 36], // size of the icon
+		shadowSize:   [82, 49], // size of the shadow
+		iconAnchor:   [32, 0],   // point of the icon which will correspond to marker's location
+		shadowAnchor: [28, 10],   // the same for the shadow
+		popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+	    });
+
 	    var map = L.map('map').setView(data, 11);
 
 	    // create the tile layer with correct attribution
@@ -10,7 +21,7 @@ $(document).ready(function (){
 	    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 14, attribution: osmAttrib});
 	    map.addLayer(osm);
 
-	    var marker = L.marker(data).addTo(map);
+	    var marker = L.marker(data, {icon: icon}).addTo(map);
 	    marker.bindPopup("<b><em>humitos</em></b> está <em>por</em> aquí!").openPopup();
 	});
     }
